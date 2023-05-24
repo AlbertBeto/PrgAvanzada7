0918217581a2df5745b1819aa13d834e66a6daec
Albert <beto@beto.cat>
2023-05-24 20:10:06 +0200

    CREAMOS UNA ENTRADA EN LA BASE DE DATOS!!!!

--- src/BaseDatos/BaseDatos.java
+++ src/BaseDatos/BaseDatos.java
@@ -3,6 +3,7 @@ package BaseDatos;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class BaseDatos {
 
@@ -10,9 +11,21 @@ public class BaseDatos {
 
         String user = "root";
         String password = "Warrilinux";
-        String url = "jdbc:mysql://localhost/PELICULAS";
+        String url = "jdbc:mysql://localhost/mydb";
 
-        try(Connection connection = DriverManager.getConnection(url,user,password)){
+        String sql = "INSERT INTO login (username, password, created) VALUES ('patri', '123', '2023-01-01 10:01:00')";
+
+        //IMPORTANTE: ESTO ES UN TRY WITH RESOURCES, QUE YA CIERRA TODA LAS CONEXIONES AUTOMATICAMENTE.
+        try(Connection connection = DriverManager.getConnection(url,user,password); Statement statement = connection.createStatement()){
+            statement.execute(sql);
+
+            /* Otra manera es esta o la mas tradicional:
+            Statement statement = connection.createStatement();
+             statement.execute(sql);
+             statement.close();
+            Cuidado que si da un error hay que hacer una movida y hay que crear catch con
+             */
+
             System.out.println(connection.getCatalog());
         }catch (SQLException e){
             System.out.println(e.getMessage());
